64bb61e57bac753a3a94760fa2d334c590e5b1d0
Shishir Halder <s.halder0108@gmail.com>
2024-06-09 09:19:32 +0600

    feat: define categories & sizcharts schema, add new column to products

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,30 @@
-import { pgTable, serial, text, doublePrecision } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  doublePrecision,
+  real,
+  integer,
+} from "drizzle-orm/pg-core";
+
+export const categories = pgTable("categories", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+});
+
+export const sizecharts = pgTable("sizecharts", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+  chest: real("chest").default(0),
+  sleeve: real("sleeve").default(0),
+  length: real("length").default(0),
+});
 
 export const products = pgTable("products", {
   id: serial("id").primaryKey(),
   name: text("name"),
   price: doublePrecision("price"),
-  description: text("description"),
+  info: text("info"),
+  categoryId: integer("categoryId").references(() => categories.id),
+  sizechartId: integer("sizechartId").references(() => sizecharts.id),
 });

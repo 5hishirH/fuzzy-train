7a82318085251c7e03751432aa8a5f663153f969
Shishir Halder <s.halder0108@gmail.com>
2024-06-09 08:32:24 +0600

    fix: use single file for all schemas

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { Hono } from "hono";
-import { products } from "../db/schemas/product.schema";
+import { products } from "../db/schema";
 
 export type Env = {
   DATABASE_URL: string;

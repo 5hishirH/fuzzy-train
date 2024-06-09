72df142f937e9b19516d3f9b30b2d73172564219
Shishir Halder <s.halder0108@gmail.com>
2024-06-09 12:45:42 +0600

    fix & feat: add pictures column to products & define stocks schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,6 +4,7 @@ import {
   text,
   doublePrecision,
   integer,
+  boolean,
 } from "drizzle-orm/pg-core";
 
 export const categories = pgTable("categories", {
@@ -21,9 +22,19 @@ export const products = pgTable("products", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   price: doublePrecision("price").notNull(),
+  pictures: text("pictures").notNull(),
   info: text("info").notNull(),
   categoryId: integer("categoryId")
     .references(() => categories.id)
     .notNull(),
   sizechartId: integer("sizechartId").references(() => sizecharts.id),
+});
+
+// temp table, will be removed later
+export const stocks = pgTable("stocks", {
+  id: serial("id").primaryKey(),
+  isStock: boolean("isStock").default(true),
+  productId: integer("productId")
+    .notNull()
+    .references(() => products.id),
 });

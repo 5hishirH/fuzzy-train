aab1deb34f34f9d39eaae8efe2d51e2f3de5772a
Shishir Halder <s.halder0108@gmail.com>
2024-06-09 11:24:17 +0600

    feat: new product, get single product, delete product

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,21 +1,70 @@
 import { Hono } from "hono";
-import { products } from "../db/schema";
+import { categories, products } from "../db/schema";
 import { Env } from "../index";
 import dbConnection from "../utils/dbConnection";
+import { eq } from "drizzle-orm";
 
 const product = new Hono<{ Bindings: Env }>();
 
-product.get("/", async (c) => {
-  try {
-    const db = dbConnection(c.env.DATABASE_URL);
+product
+  .get("/", async (c) => {
+    try {
+      const db = dbConnection(c.env.DATABASE_URL);
 
-    const result = await db.select().from(products);
+      const result = await db.select().from(products);
 
-    return c.json(result);
-  } catch (error) {
-    console.log(error);
-    return c.json({ error: "Interal server error!" }, 500);
-  }
-});
+      return c.json(result);
+    } catch (error) {
+      console.log(error);
+      return c.json({ error: "Interal server error!" }, 500);
+    }
+  })
+  .get("/:id", async (c) => {
+    try {
+      const db = dbConnection(c.env.DATABASE_URL);
+
+      const productId = parseInt(c.req.param("id"));
+
+      const result = await db
+        .select()
+        .from(products)
+        .innerJoin(categories, eq(products.id, productId));
+
+      return c.json(result[0]);
+    } catch (error) {
+      console.log(error);
+      return c.json({ error: "Interal server error!" }, 500);
+    }
+  })
+  .post("/new", async (c) => {
+    try {
+      const db = dbConnection(c.env.DATABASE_URL);
+
+      const body = await c.req.json();
+      const newProduct = await db.insert(products).values(body).returning();
+
+      return c.json(newProduct);
+    } catch (error) {
+      console.log(error);
+      return c.json({ error: "Interal server error!" }, 500);
+    }
+  })
+  .delete("/delete/:id", async (c) => {
+    try {
+      const db = dbConnection(c.env.DATABASE_URL);
+
+      const productId = parseInt(c.req.param("id"));
+
+      const result = await db
+        .delete(products)
+        .where(eq(products.id, productId))
+        .returning();
+
+      return c.json(result);
+    } catch (error) {
+      console.log(error);
+      return c.json({ error: "Interal server error!" }, 500);
+    }
+  });
 
 export default product;

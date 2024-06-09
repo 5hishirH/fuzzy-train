1251fec2d371169ab882418f57910914c53dc563
Shishir Halder <s.halder0108@gmail.com>
2024-06-09 12:47:19 +0600

    feat: add stocks route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import product from "./routes/product";
 import category from "./routes/category";
 import sizechart from "./routes/sizechart";
+import stock from "./routes/stocks";
 
 export type Env = {
   DATABASE_URL: string;
@@ -16,5 +17,6 @@ app.get("/", (c) => {
 app.route("/category", category);
 app.route("/sizechart", sizechart);
 app.route("/product", product);
+app.route("/stock", stock);
 
 export default app;

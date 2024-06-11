a14a65e13879717bcb32fa282e7d8be2836a3869
Shishir Halder <s.halder0108@gmail.com>
2024-06-11 16:16:47 +0600

    feat: quantity for single product and add a new cors origin
    new cors origin: https://journeyy-clothes.vercel.app

--- src/routes/quantity.ts
+++ src/routes/quantity.ts
@@ -1,23 +1,37 @@
-import { Hono } from "hono";
-import { Env } from "../index";
+import {Hono} from "hono";
+import {Env} from "../index";
 import dbConnection from "../utils/dbConnection";
-import { quantities } from "../db/schema";
+import {quantities} from "../db/schema";
+import {and, eq, gt} from "drizzle-orm"
 
 const quantity = new Hono<{ Bindings: Env }>();
 
-quantity.post("/new", async (c) => {
-  try {
-    const db = dbConnection(c.env.DATABASE_URL);
+quantity.get("/:id",
+    async (c) => {
+        try {
+            const db = dbConnection(c.env.DATABASE_URL);
+            const productId = parseInt(c.req.param("id"));
+            const result = await db.select().from(quantities).where(and(eq(quantities.productId, productId), gt(quantities.quantity, 0)));
 
-    const body = await c.req.json();
+            return c.json(result);
+        } catch (error) {
+            console.log(error);
+            return c.json({error: "Internal server error"}, 500);
+        }
+    }
+).post("/new", async (c) => {
+    try {
+        const db = dbConnection(c.env.DATABASE_URL);
 
-    const result = await db.insert(quantities).values(body).returning();
+        const body = await c.req.json();
 
-    return c.json(result);
-  } catch (error) {
-    console.log(error);
-    return c.json({ error: "Internal server error!" }, 500);
-  }
+        const result = await db.insert(quantities).values(body).returning();
+
+        return c.json(result);
+    } catch (error) {
+        console.log(error);
+        return c.json({error: "Internal server error!"}, 500);
+    }
 });
 
 export default quantity;

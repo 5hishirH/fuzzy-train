585b1aead5eb6701a542fa5030bbfb8df5a9e892
Shishir Halder <s.halder0108@gmail.com>
2024-06-25 08:04:39 +0600

    feat: use drizzle query

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -3,7 +3,7 @@ import { categories, products, quantities } from "../db/schema";
 import * as schema from "../db/schema";
 import { Env } from "../index";
 import dbConnection from "../utils/dbConnection";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
@@ -16,6 +16,7 @@ product
       const db = drizzle(sql, { schema });
 
       const result = await db.query.products.findMany({
+        orderBy: [desc(products.id)],
         columns: {
           categoryId: false,
           sizechartId: false,
@@ -41,24 +42,31 @@ product
   })
   .get("/:id", async (c) => {
     try {
-      const db = dbConnection(c.env.DATABASE_URL);
+      const sql = neon(c.env.DATABASE_URL);
+      const db = drizzle(sql, { schema });
 
       const productId = parseInt(c.req.param("id"));
 
-      const result = await db
-        .select({
-          id: products.id,
-          name: products.name,
-          categoryName: categories.name,
-          categoryId: categories.id,
-          price: products.price,
-          pictures: products.pictures,
-          info: products.info,
-        })
-        .from(products)
-        .innerJoin(categories, eq(products.id, productId));
+      const result = await db.query.products.findFirst({
+        where: eq(products.id, productId),
+        columns: {
+          categoryId: false,
+          sizechartId: false,
+        },
+        with: {
+          category: true,
+          sizechart: true,
+          quantities: {
+            columns: {
+              id: true,
+              size: true,
+              isStock: true,
+            },
+          },
+        },
+      });
 
-      return c.json(result[0]);
+      return c.json(result);
     } catch (error) {
       console.log(error);
       return c.json({ error: "Internal server error!" }, 500);
